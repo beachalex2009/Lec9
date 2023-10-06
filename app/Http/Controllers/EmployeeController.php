a9881b7d992edfaab088bf2fa0d5664349a7ca2b
beachalex2009 <beach_alex2009@yahoo.com>
2023-10-06 10:35:29 +0300

    'update'

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends Controller
+{
+
+    public function index()
+
+    {
+        // dd($employee = DB::select('select * from employees'));
+        // dd($employee=DB::insert ("INSERT INTO regions (REGION_ID,REGION_NAME) VALUES (? , ?)" , [10 , 'insert from raghda']));
+        //  dd($employee=DB::update("update regions set REGION_NAME='Raghda' where REGION_ID=10"));
+         dd($employee=DB::delete("delete from regions where REGION_ID=10"));
+        return view('employees',compact($employee));
+    }
+}
